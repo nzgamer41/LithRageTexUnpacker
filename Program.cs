7a0c1532645f090cb60c1eed81c13aab2124e881
nzgamer41 <pslover9@gmail.com>
2024-08-22 22:10:03 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using SixLabors.ImageSharp;
+
+namespace LithRageTexUnpacker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: LithRAGETexUnpack.exe <path to texture> <path to palette>");
+                return;
+            }
+            else
+            {
+                string fileName = Path.GetFileNameWithoutExtension(args[0]);
+                var texture = TextureLoader.LoadTexture(args[0], args[1]);
+                texture.Save($"{fileName}.png");
+                return;
+            }
+
+        }
+    }
+}
---
+++ TextureLoader.cs
@@ -0,0 +1,50 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+
+public class TextureLoader
+{
+    public static Image<Rgba32> LoadTexture(string textureFilePath, string paletteFilePath)
+    {
+        using (FileStream textureStream = new FileStream(textureFilePath, FileMode.Open, FileAccess.Read))
+        using (FileStream paletteStream = new FileStream(paletteFilePath, FileMode.Open, FileAccess.Read))
+        using (BinaryReader textureReader = new BinaryReader(textureStream))
+        using (BinaryReader paletteReader = new BinaryReader(paletteStream))
+        {
+            // Read width and height
+            //height is byte 1, width is byte 2
+            int height = textureReader.ReadInt16();
+            int width = textureReader.ReadInt16();
+
+            // Read the palette
+            Rgba32[] palette = new Rgba32[256];
+            for (int i = 0; i < 256; i++)
+            {
+                byte r = paletteReader.ReadByte();
+                byte g = paletteReader.ReadByte();
+                byte b = paletteReader.ReadByte();
+                palette[i] = new Rgba32(r, g, b); // 24-bit color
+            }
+
+            // Read the texture data
+            byte[] textureData = textureReader.ReadBytes(width * height);
+
+            // Create a new Image<Rgba32>
+            var image = new Image<Rgba32>(height, width);
+
+            // Map the texture data to the palette and create the image, correcting the vertical flip
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    int index = textureData[y * width + x];
+                    image[y, x] = palette[index];
+                }
+            }
+
+            return image;
+        }
+    }
+}
